773d2f778e62d10250c7806e5ffd10f311fdebc6
biplob-kb <biplobkb123@gmail.com>
2026-06-02 19:35:58 +0600

    scaffold completed

---
+++ 01_minimal_api/DbContexts/AuthDbContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using _01_minimal_api.Models.Entities;
+
+namespace _01_minimal_api.DbContexts;
+
+public partial class AuthDbContext : DbContext
+{
+    public AuthDbContext()
+    {
+    }
+
+    public AuthDbContext(DbContextOptions<AuthDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__Users__1788CC4C1C935700");
+
+            entity.HasIndex(e => e.Email, "UQ__Users__A9D10534560F8CB4").IsUnique();
+
+            entity.Property(e => e.UserId).HasDefaultValueSql("(newid())");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(50);
+            entity.Property(e => e.Password).HasMaxLength(250);
+            entity.Property(e => e.RoleId).HasDefaultValue(1);
+            entity.Property(e => e.UserName).HasMaxLength(200);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ 01_minimal_api/Models/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_minimal_api.Models.Entities;
+
+public partial class User
+{
+    public Guid UserId { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public int? RoleId { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+}
